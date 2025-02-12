92c8e5dd0c8975be3cf832c1dffa780c3034dccb
cileene <106276740+cileene@users.noreply.github.com>
2025-02-12 17:27:42 +0100

    Add sceneloading

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] private string sceneToLoad;
+    [SerializeField, Range(0.1f, 1.5f)] private float transitionTime = 0.5f;
     private string _buttonName;
-    
+    private GameObject _transitionCamera;
+
     private void Awake()
     {
         _buttonName = gameObject.name;
+        _transitionCamera = transform.Find("TransitionCamera").gameObject;
     }
-    
+
     private void OnMouseDown()
     {
         // set material color to grey
@@ -18,8 +23,28 @@ public class Button : MonoBehaviour
 
     private void OnMouseUp()
     {
+        // activate the transition camera
+        if (!string.IsNullOrEmpty(sceneToLoad)) _transitionCamera.SetActive(true);
+
         // set material color to white
         GetComponent<Renderer>().material.color = Color.white;
         Debug.Log($"{_buttonName} released!");
+
+        // wait for transitionTime seconds and then load the scene
+        Invoke(nameof(LoadScene), transitionTime);
+    }
+
+    private void LoadScene()
+    {
+        // if the scene to load is not empty, load the scene
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            Debug.Log($"Loading scene: {sceneToLoad}");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("Scene to load is not specified.");
+        }
     }
 }
