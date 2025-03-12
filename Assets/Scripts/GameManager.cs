105d15aea4e5a2a1c7eeef1de33058546b42a7c3
cileene <106276740+cileene@users.noreply.github.com>
2025-03-12 10:54:30 +0100

    Refactor save data initialization and add read functionality in GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,9 @@ using System;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    
-    [Header("Progression")]
-    public bool phoneUnlocked;
-    
+
+    [Header("Progression")] public bool phoneUnlocked;
+
     private string _dataPath;
     private string _saveData;
     private const string FileName = "SaveData.txt";
@@ -23,48 +22,52 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
             InitFileSystem();
-            InitTextFile();
+            InitSaveData();
             InitAnalytics();
             UpdateSaveData($"Game started: {DateTime.Now}");
+            ReadSaveData(_saveData);
         }
         else
         {
             Destroy(gameObject);
         }
     }
-    
+
     //TODO: we want to track the current open scene to make sure it will be opened on app restart
-    
+    //TODO: P2-57 refactor to use json serialization (chapter 12 in the book)
+
     private void InitFileSystem()
     {
         _dataPath = Application.persistentDataPath + "/Player_Data/";
         Debug.Log(_dataPath);
-        
-        if(Directory.Exists(_dataPath))
+
+        if (Directory.Exists(_dataPath))
         {
             Debug.Log($"Directory already exists at {_dataPath}");
             return;
         }
+
         Directory.CreateDirectory(_dataPath);
         Debug.Log($"New directory created at {_dataPath}");
     }
-    
+
     private void InitAnalytics()
     {
         Instantiate(Resources.Load("Prefabs/UnityAnalytics"));
         Debug.Log("Analytics initialized");
     }
-    
-    private void InitTextFile()
+
+    private void InitSaveData()
     {
         _saveData = _dataPath + FileName;
-        
+
         if (File.Exists(_saveData))
         {
             Debug.Log($"File already exists at {_saveData}");
             return;
         }
-        File.WriteAllText(_saveData, "<SAVE DATA>\n ");
+
+        File.WriteAllText(_saveData, "<SAVE DATA>\n");
         Debug.Log($"New file created at {_saveData}");
     }
 
@@ -75,6 +78,19 @@ public class GameManager : MonoBehaviour
             Debug.LogWarning("File does not exist!");
             return;
         }
+
         File.AppendAllText(_saveData, data + "\n");
+    }
+
+    private void ReadSaveData(string saveData)
+    {
+        if (!File.Exists(saveData))
+        {
+            Debug.LogWarning("File does not exist!");
+            return;
+        }
+
+        string text = File.ReadAllText(saveData);
+        Debug.Log(text);
     }
 }
