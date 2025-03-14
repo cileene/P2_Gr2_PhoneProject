83d3018a792bfb9d3026987a20931556c5c0a6fd
cileene <106276740+cileene@users.noreply.github.com>
2025-03-14 22:22:14 +0100

    Add parallax effect to homescreen BG

---
+++ Assets/Scripts/GyroParallax.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GyroParallax : MonoBehaviour
+{
+    [Tooltip("The offset amount for the parallax effect")]
+    [SerializeField] private float parallaxIntensity = 0.05f;
+    [SerializeField] private float smoothSpeed = 5.0f;
+
+    private bool _gyroAvailable;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            _gyroAvailable = true;
+            _startPosition = transform.localPosition;
+        }
+        else
+        {
+            Debug.LogError("Gyroscope not supported on this device.");
+        }
+    }
+
+    private void Update()
+    {
+        if (!_gyroAvailable) return;
+
+        Quaternion gyro = Input.gyro.attitude;
+        Vector3 gyroMovement = new Vector3(gyro.y, gyro.x, 0) * parallaxIntensity;
+
+        Vector3 targetPosition = _startPosition + gyroMovement;
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * smoothSpeed);
+    }
+}
