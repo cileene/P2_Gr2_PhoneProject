1d4985c5b47ebff0f1c62dd04a01b91f284e1937
ValdemarWessing <iq62FM@student.aau.dk>
2025-03-15 21:28:01 +0100

    AddMessagingWithSavedData

---
+++ Assets/Scripts/ChoiceButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ChoiceButton : MonoBehaviour
+{
+    private MessageManager _choiceManager;  // Reference to the main dialogue manager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the main dialogue manager in the scene
+        _choiceManager = FindObjectOfType<MessageManager>();
+    }
+
+    // This method gets called when the sprite is clicked and the mouse button is released
+    void OnMouseUp()
+    {
+        if (_choiceManager != null)
+        {
+            // Determine which choice this sprite represents (1 or 2)
+            if (gameObject.name == "ChoiceSprite1") // For Sprite 1
+            {
+                _choiceManager.OnChoiceSprite1Clicked();  // Call the method from the main manager
+            }
+            else if (gameObject.name == "ChoiceSprite2") // For Sprite 2
+            {
+                _choiceManager.OnChoiceSprite2Clicked();  // Call the method from the main manager
+            }
+        }
+    }
+}
