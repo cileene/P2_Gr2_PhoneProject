53531933aa619ad0a1a89799315ccd9a4bf92798
na-noir <nanna-friis@live.dk>
2025-03-24 20:25:49 +0100

    BedrePhotoAlbum

---
+++ Assets/Scripts/ZoomManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomManager : MonoBehaviour
+{
+    public GameObject zoomPanel;
+    public Image zoomedImage;
+    public Image fullScreenImage;
+
+    public void OpenZoomView()
+    {
+        zoomedImage.sprite = fullScreenImage.sprite;
+        zoomPanel.SetActive(true);
+    }
+
+    public void CloseZoomView()
+    {
+        zoomPanel.SetActive(false);
+    }
+}
