d68c86c29a08e47201398a07f27c58655aaf5fa0
cileene <106276740+cileene@users.noreply.github.com>
2025-04-24 12:26:36 +0200

    Added cancel button to idmoji

--- Assets/Scripts/IDMojiApp/IdMojiLogic.cs
+++ Assets/Scripts/IDMojiApp/IdMojiLogic.cs
@@ -5,18 +5,17 @@ namespace IDMojiApp
     public class IdMojiLogic : MonoBehaviour
     {
         [SerializeField] private GameObject popUpPanel;
-        
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         public void Confirm()
         {
+            if (GameManager.Instance.iDMojiCreated) return;
+            
+            GameManager.Instance.iDMojiCreated = true;
             GameManager.Instance.progressStory = true;
             popUpPanel.SetActive(true);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+
     }
 }
